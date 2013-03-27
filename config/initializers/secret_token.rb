c43380048eb59c375640bda086b6eff80daac692
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-27 15:40:08 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = 'fcd2691c625363e0d72c45ef593dfc07b409c9d3faf56c8dc32ec5a7fbca1c1aba5e81f0cd6d538ffe6b620fcf2d5453df8365e5eacd0948c382d2b01c531739'
